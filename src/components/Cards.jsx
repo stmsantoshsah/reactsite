5616f3420d6211ec4c4010cf3859b8c37f4ca968
santosh <yousilenceplease@gmail.com>
2024-06-18 01:09:19 +0530

    first commit

---
+++ src/components/Cards.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function Cards() {
+  return (
+    <div className='cards'>
+      <div className="card">
+        <div className="card-img">
+            <img src="https://picsum.photos/300/300" alt="" />
+        </div>
+        <div className="card-details">
+            <h3 className="card-title">First Card</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, molestias.</p>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card-img">
+            <img src="https://picsum.photos/300/300" alt="" />
+        </div>
+        <div className="card-details">
+            <h3 className="card-title">First Card</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, molestias.</p>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card-img">
+            <img src="https://picsum.photos/300/300" alt="" />
+        </div>
+        <div className="card-details">
+            <h3 className="card-title">First Card</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, molestias.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Cards
